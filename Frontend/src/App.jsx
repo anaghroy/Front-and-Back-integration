35b42f83ea359ed3f7c45b3d84acf0bb970aed17
AnaghRoy <royanagh1999@gmail.com>
2026-02-03 14:05:23 +0530

    created delete and create note functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-
+import { Trash2 } from "lucide-react";
 function App() {
   const [notes, setNotes] = useState([
     {
@@ -29,14 +29,74 @@ function App() {
     },
   ]);
 
-  axios
-    .get("http://localhost:3000/api/note") /**Fetching all data from backend */
-    .then((res) => {
-      setNotes(res.data.note);
-    });
+  /**All Notes */
+  function fetchNotes() {
+    axios
+      .get(
+        "http://localhost:3000/api/note",
+      ) /**Fetching all data from backend */
+      .then((res) => {
+        setNotes(res.data.note);
+      });
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  /**Create notes */
+  function createNotes(e) {
+    e.preventDefault();
+
+    const { title, age, description, place } = e.target.elements;
+
+    axios
+      .post("http://localhost:3000/api/note", {
+        title: title.value,
+        age: age.value,
+        description: description.value,
+        place: place.value,
+      })
+      .then((res) => {
+        fetchNotes();
+      });
+  }
+
+  /**Delete by note ID */
+  function handleDeleteNote(noteId) {
+    axios
+      .delete("http://localhost:3000/api/note/"+noteId, )
+      .then((res) => {
+        // console.log(res.data)
+        fetchNotes();
+      });
+  }
 
   return (
     <>
+      <form className="create-note-form" onSubmit={createNotes}>
+        <input
+          name="title"
+          type="text"
+          placeholder="Enter your title"
+          required
+        />
+        <input name="age" type="number" placeholder="Enter your age" required />
+        <input
+          name="description"
+          type="text"
+          placeholder="Enter your description"
+          required
+        />
+        <input
+          name="place"
+          type="text"
+          placeholder="Enter your place"
+          required
+        />
+        <button>Create Note</button>
+      </form>
+
       <div className="notes">
         {notes.map((note, index) => {
           return (
@@ -45,6 +105,16 @@ function App() {
               <span>{note.age}</span>
               <p>{note.description}</p>
               <span>{note.place}</span>
+              <div className="del">
+                <button
+                  onClick={() => {
+                    handleDeleteNote(note._id);
+                  }}
+                  className="delete"
+                >
+                  <Trash2 />
+                </button>
+              </div>
             </div>
           );
         })}
